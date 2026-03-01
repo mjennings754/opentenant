513a7444fbbe177b39a15d81e7687e018833265a
mjennings754 <mitchellj@outlook.jp>
2026-03-02 07:50:35 +1100

    add address method for property

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -22,6 +22,7 @@ class PropertiesController < ApplicationController
   end
 
   def show
+    @property = Property.find(params[:id])
   end
 
   private
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,13 @@
 class Property < ApplicationRecord
   belongs_to :organization
+
+  def address
+    [
+      address_line_one,
+      address_line_two,
+      city,
+      state,
+      postcode
+    ].reject(&:blank?).join(', ')
+end
 end
