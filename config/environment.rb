bd5d24bf113ba1e5ca9573a670a7e45d3f47767f
mjennings754 <mitchellj@outlook.jp>
2026-03-02 16:27:51 +1100

    add logger info

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Rails.logger = Logger.new(STDOUT)
-
-Rails.logger.level = Logger::DEBUG
-Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
-
-Rails.logger.formatter = proc do |severity, time, progname, msg|
-    formatted_time = time.strftime(Rails.logger.datetime_format)
-    "#{formatted_time}, #{severity}: #{msg} from #{progname}\n"
-end
