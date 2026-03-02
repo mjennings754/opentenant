cb4b10e4d73f648e80f718c9b6039fbf423c796d
mjennings754 <mitchellj@outlook.jp>
2026-03-03 08:23:53 +1100

    show current_tenant the property

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   before_action :require_login
   def home
     @organizations = current_user.all_organizations
+    @properties = current_user.properties
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :members
   has_many :organizations, through: :members
   has_many :created_organizations, class_name: "Organization", foreign_key: :user_id
-
+  has_many :tenants
+  has_many :properties, through: :tenants
   def all_organizations
     organizations.or(created_organizations).distinct
   end
