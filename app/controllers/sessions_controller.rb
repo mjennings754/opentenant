6d27cf836092f0d909081a99f1b8858f1fcafb6f
mjennings754 <mitchellj@outlook.jp>
2026-03-05 14:28:10 +1100

    add logout message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_path
+    redirect_to login_path, notice: "You have been successfully logged out"
   end
 end
