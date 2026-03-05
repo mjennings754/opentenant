f13442bcdb005274fb5b338ff3a6563cba1e4251
mjennings754 <mitchellj@outlook.jp>
2026-03-05 16:59:48 +1100

    add lograge

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  config.lograge.enabled = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
